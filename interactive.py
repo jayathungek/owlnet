67d404ab7062c71134fab496f8a6b955cc377ee9
jayathungek <jayathungek@gmail.com>
2025-03-04 19:32:49 +0000

    fixed bug with dataset slider

--- interactive.py
+++ interactive.py
@@ -25,14 +25,14 @@ def create_embeds(model, dataloader):
 # Control flag
 auto_run = False
 total_ds_size = 3375
-hop_size = 20
+hop_size = 50
 iteration = 0
 num_iterations = total_ds_size // hop_size
 collate_func = CollateFunc(spec_height=750)
 window_start = 0
 
 # Buttons
-progress = widgets.Label(value=f"█{'░'* (num_iterations)}" )
+progress = widgets.Label(value=f"█{'░'* (num_iterations - 1)}" )
 step_button = widgets.Button(description="Step")
 reset_button = widgets.Button(description="Reset")
 
@@ -41,6 +41,9 @@ def step_run(dataset, model, visualiser):
     global auto_run
     global iteration
     global num_iterations
+
+    if iteration > num_iterations - 1:
+        iteration = 0
 
     auto_run = False
     bar = ["░"] * num_iterations# Reset bar
@@ -55,7 +58,7 @@ def reset(visualiser, progress_widget):
     global iteration
     global auto_run
     iteration = 0
-    progress_widget.value = f"█{'░'* (num_iterations)}" 
+    progress_widget.value = f"█{'░'* (num_iterations - 1)}" 
     auto_run = False
     visualiser.pop_verification_trace()
 
@@ -65,9 +68,9 @@ def loop_iteration(owlet_dataset, owlnet, visualiser):
     global iteration
     global hop_size
     global total_ds_size
+    global num_iterations
     global collate_func
-    if iteration >= total_ds_size // hop_size:
-        return
+
     start = iteration * hop_size
     hop_size = min(total_ds_size - start, hop_size)
     indices = [start, start + hop_size]
